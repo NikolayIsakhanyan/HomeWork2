26c2baf94ca25a90664d81d7aa4fddab5b4943a6
Isakhanyan Nikolay <isaxanyan23@outlook.com>
2022-11-29 20:15:18 +0400

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations;
+
+
+//int[] array1 = { 1,5,5,5,5};
+//int[] array2 = { 4,8,6,2,4 };
+//int sum1 = 0;
+//int sum2 = 0;
+//for (int i=0;i<array1.Length; i++)
+//{
+//    sum1 += array1[i];
+//    sum2 += array2[i];
+//}
+//Console.WriteLine((sum1*sum2)/(array1.Length*array2.Length));
+
+int n;
+int [] aray =new int [int.MaxValue];
+do
+{
+    Console.WriteLine("n=");
+    n = Convert.ToInt32(Console.ReadLine());
+} while (n < 1 && n > 20);
+for(int i = 0; i < n; i++)
+{
+    aray[i]=  Convert.ToInt32(Console.ReadLine());
+    if (aray[i])
+}

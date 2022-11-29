711a4189513896ce86c35b4c22d0d2e34b74aa54
Isakhanyan Nikolay <isaxanyan23@outlook.com>
2022-11-29 21:59:16 +0400

    H JCV

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -13,23 +13,63 @@ using System.ComponentModel.DataAnnotations;
 //}
 //Console.WriteLine((sum1*sum2)/(array1.Length*array2.Length));
 
-int n;
-int [] aray =new int [20];
-do
+//int n;
+//int [] aray =new int [20];//291
+//do
+//{
+//    Console.Write("n=");
+//    n = Convert.ToInt32(Console.ReadLine());
+//} while (n < 1 && n > 20);
+//for(int i = 0; i < n; i++)
+//{
+//    Console.Write("  aray["+i+"]=");
+//    aray[i]=  Convert.ToInt32(Console.ReadLine());
+
+//}
+//for (int i = 0; i < n; i++)
+//{
+//    if (aray[i] > 0)
+//    {
+//        Console.WriteLine("  aray[" +i+"]=" +aray[i]);
+//    }
+//}
+
+
+NewMethod();
+
+static void NewMethod()
 {
-    Console.Write("n=");
-    n = Convert.ToInt32(Console.ReadLine());
-} while (n < 1 && n > 20);
-for(int i = 0; i < n; i++)
-{
-    Console.Write("  aray["+i+"]=");
-    aray[i]=  Convert.ToInt32(Console.ReadLine());
-    
-}
-for (int i = 0; i < n; i++)
-{
-    if (aray[i] > 0)
+    int[] array1 = new int[20];
+    int[] array2 = new int[40];
+    int n;
+    int j = -1;
+    do
     {
-        Console.WriteLine("  aray[" +i+"[=" +aray[i]);
+        n = Convert.ToInt32(Console.ReadLine());
+    } while (n < 1 || n > 20);
+    for (int i = 0; i < n; i++) 
+    {
+
+        Random rand = new Random();
+        array1[i]=rand.Next(-100,0);
+
     }
+    for (int i = 0; i < n; i++)
+    {
+        j++;
+        array2[j] = array1[i];
+        if (array1[i] < 0)
+        {
+            j++;
+            for (int k = 0; k <= j; k++)
+            {
+                array2[k] = 0;
+            }
+        }
+    }
+    for(int i=0; i<=j; i++)
+    {
+        Console.WriteLine(array2[i]);
+    }
+    Console.ReadKey();
 }
